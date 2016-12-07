8f3073226d5bf61e1709440042a780b4e4cd08ee
larry <larry@larrys-macbook-air.local>
2016-12-07 07:21:29 -0800

    Initial Commit

---
+++ GoGameRecorder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GoGameRecorder
+//
+//  Created by larry on 12/7/16.
+//  Copyright © 2016 larry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
