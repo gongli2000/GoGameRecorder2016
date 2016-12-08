47b0f35865cab3a0b9aadbb7c0c18ccbe712c99d
larry <larry@larrys-macbook-air.local>
2016-12-08 12:11:40 -0800

    add transform board

---
+++ GoGameRecorder/transformBoard.cpp
@@ -0,0 +1,9 @@
+//
+//  transformBoard.cpp
+//  GoGameRecorder
+//
+//  Created by larry on 12/8/16.
+//  Copyright © 2016 larry. All rights reserved.
+//
+
+#include "transformBoard.hpp"
---
+++ GoGameRecorder/transformBoard.hpp
@@ -0,0 +1,14 @@
+//
+//  transformBoard.hpp
+//  GoGameRecorder
+//
+//  Created by larry on 12/8/16.
+//  Copyright © 2016 larry. All rights reserved.
+//
+
+#ifndef transformBoard_hpp
+#define transformBoard_hpp
+
+#include <stdio.h>
+
+#endif /* transformBoard_hpp */
