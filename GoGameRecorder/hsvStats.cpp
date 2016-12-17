bf1fb68ac06f8da04f1c3b8301022585c2c894b7
larry <larry@larrys-macbook-air.local>
2016-12-16 21:53:49 -0800

    fixes

--- GoGameRecorder/hsvStats.cpp
+++ GoGameRecorder/hsvStats.cpp
@@ -42,8 +42,10 @@ void Get_Threshold_Values()
     
     for(;;)
     {
-        Mat gray;
+        Mat gray,savegray;
         videocam >> gray;
+        Mat blankimage = gray.clone().setTo(Scalar(255,255,255));
+        savegray = gray.clone();
         namedWindow( "Gray", 1 );
       
         cvtColor(gray, gray, CV_BGR2HSV);
@@ -55,24 +57,45 @@ void Get_Threshold_Values()
         vector<Mat> inputimage = {gray},images;
         Scalar means,stddev;
         meanStdDev(gray, means,stddev);
-        printf("h,s,v means %f.2,%f.2 %f.2\n",means[0],means[1],means[2]);
-        printf("h,s,v std %f.2,%f.2 %f.2\n\n",stddev[0],stddev[1],stddev[2]);
-        
+        char minmean[256], maxmean[256];
+    
+    
         for(int i =0;i<3;i++){
             Mat hist, histout;
             vector<int> channels = {i};
             calcHist(inputimage,channels,Mat(),hist,histSize,ranges);
             histout =plothistogram(hist,histSize[0]);
+
             cvtColor(histout,histout,CV_GRAY2BGR);
             images.push_back(histout);
         }
-        images.push_back(gray);
+       
+       
         
         Mat threshold_frame;
-        inRange(gray, Scalar(220/2, 0, 0), Scalar(260/2, 255, 255), threshold_frame);
+        double delta = 20;
+        Scalar minmeans = means - Scalar(delta,delta,delta);
+        Scalar maxmeans = means + Scalar(delta,delta,delta);
+
+        minmeans = Scalar(0,0,-20);
+        maxmeans = Scalar(255,255,60);
+        inRange(gray, minmeans, maxmeans, threshold_frame);
         cvtColor(threshold_frame,threshold_frame,CV_GRAY2BGR);
-        images.push_back(threshold_frame);
         
+        sprintf(minmean,"min %.2f,  %.2f, %.2f\n",
+                minmeans[0],minmeans[1],minmeans[2]);
+        sprintf(maxmean,"max %.2f, %.2f , %.2f\n\n",maxmeans[0],maxmeans[1],maxmeans[2]);
+        
+        putText(blankimage, minmean, Point(50,100),FONT_HERSHEY_PLAIN, 4,
+            Scalar(0,0,255), 3, 3);
+        putText(blankimage, maxmean, Point(50,200),FONT_HERSHEY_PLAIN, 4,
+                Scalar(0,0,255), 3, 3);
+        printf("%.2f, %.2f, %.f\n",minmeans[0],minmeans[1],minmeans[2]);
+          printf("%.2f, %.2f, %.f\n\n",maxmeans[0],maxmeans[1],maxmeans[2]);
+        
+         images.push_back(savegray);
+        images.push_back(threshold_frame);
+        images.push_back(blankimage);
         Mat out =concatMats(images, 2, 3, 300, 300);
         imshow( "Gray", out );
         waitKey(1);
